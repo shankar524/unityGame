e6dfbf38eb8529e21ba2802b4a7f05afe8018469
Shankar Acharya <shankar.acharya524@gmail.com>
2017-03-30 17:08:11 +0545

    first commit

---
+++ Assets/Script/DataBaseManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DataBaseManager : MonoBehaviour {
+	public string DomainName,urlServer;
+	public Text username, email;
+	// Use this for initialization
+	void Start () {
+		urlServer = DomainName + "/unitybootcamp/login.php";
+		//DomainName="192.168.1.24";
+	}
+	public void UserPush(){
+		StartCoroutine ("Connect");
+	}
+
+	//for asynchronous function
+	IEnumerator Connect()
+	{
+		WWWForm form = new WWWForm ();
+		form.AddField ("username", username.text.ToString ());
+		form.AddField ("email", email.text.ToString ());
+		WWW w = new WWW (urlServer, form);
+		//for animation we can use yoels new WaitfroSeconds
+		yield return w;//it prevents from futher code proceed for slow answer receive
+		if (!string.IsNullOrEmpty (w.error)) {
+			Debug.Log (w.text.ToString());
+		} else {
+			Debug.Log (w.text.ToString());
+		}
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Script/Play.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Play : MonoBehaviour {
+	private GameObject settingTab;
+	public void LoadScene(string s){
+		SceneManager.LoadScene (s, LoadSceneMode.Single);
+	}
+	public void LoadPanel()
+	{
+		//GameObject settingTab=GameObject.FindGameObjectWithTag("settingTag");
+		settingTab.SetActive (true);
+	}
+
+	// Use this for initialization
+	void Start () {
+		settingTab=GameObject.FindGameObjectWithTag("settingTag");
+		settingTab.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+//hello k xa khabbar
